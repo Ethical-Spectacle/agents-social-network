1b49ee500e3a443e8b7e97aad85e487f940336ec
Maximus Powers <srewop.xam@gmail.com>
2024-06-22 15:38:03 -0700

    Relevance check/suggest statement completed

--- testAgentInteraction.py
+++ testAgentInteraction.py
@@ -8,20 +8,21 @@ network_id = "grapevine-test"
 db.create_network(network_id, "Grapevine Test", "Internal test network")
 
 # create two agents
-home_agent_id = db.create_agent("grapevine-test")
-away_agent_id = db.create_agent("grapevine-test")
+# home_agent_id = db.create_agent("grapevine-test")
+# away_agent_id = db.create_agent("grapevine-test")
 
-# home_agent_id = "1"
-# away_agent_id = "2"
+home_agent_id = "1"
+away_agent_id = "2"
 
-db.add_agent_data(home_agent_id, "micheal, a software engineer, is working on a new project with agents, and is looking for people to join his team.")
-db.add_agent_data(home_agent_id, "maximus, is a tall guy who plays basketball at ASU.")
+# db.add_agent_data(home_agent_id, "micheal, a software engineer, is working on a new project with agents, and is looking for people to join his team.")
+# db.add_agent_data(home_agent_id, "maximus, is a tall guy who plays basketball at ASU.")
 
-db.add_agent_data(away_agent_id, "jane, a data scientist in Phoenix, just graduated and is looking for a job.")
-db.add_agent_data(away_agent_id, "mark, a man who loves to travel, is planning a trip to Europe.")
+# db.add_agent_data(away_agent_id, "jane, a data scientist in Phoenix, just graduated and is looking for a job.")
+# db.add_agent_data(away_agent_id, "mark, a man who loves to travel, is planning a trip to Europe.")
 
 # init the agents (make sure to pass the db object)
-home_agent = Agent(network_id=network_id, agent_id=home_agent_id, db=db)
+home_agent = Agent(network_id=network_id, agent_id=home_agent_id, db=db) 
+
 
 # func to run the test interaction
 def run_test_interaction():
